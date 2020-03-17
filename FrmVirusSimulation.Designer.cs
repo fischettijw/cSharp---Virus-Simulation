17b460c1de1a80cfcb62d89ba393a554f1da286d
Joseph Fischetti <fischettijw@gmail.com>
2020-03-17 05:47:33 -0400

    Add project files.

---
+++ FrmVirusSimulation.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace cSharp___Virus_Simulation
+{
+    partial class FrmVirusSimulation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmVirusSimulation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FrmVirusSimulation";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmVirusSimulation_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ FrmVirusSimulation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharp___Virus_Simulation
+{
+    public partial class FrmVirusSimulation : Form
+    {
+        public FrmVirusSimulation()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmVirusSimulation_Load(object sender, EventArgs e)
+        {
+                       CusttomizeForm();
+        }
+
+        private void CusttomizeForm()
+        {
+            this.Hide();
+            this.Text = "Coronavirus - Small Gathering Simulation";
+
+            int clientWidth = 400;
+            int clientHeight = clientWidth + 100;
+            this.ClientSize = new Size(clientWidth, clientHeight);
+            this.MaximizeBox = false;
+            this.MaximumSize = new Size(this.Width, this.Height);
+            this.MinimizeBox = true;
+            this.MinimumSize = new Size(this.Width, this.Height);
+
+            this.Icon = Properties.Resources.virus_image;
+
+            int sW = Screen.PrimaryScreen.WorkingArea.Width;
+            int sH = Screen.PrimaryScreen.WorkingArea.Height;
+            int fW = this.Width;
+            int fH = this.Height;
+            this.Location = new Point((sW - fW) / 2, (sH - fH) / 2);
+
+            this.Show();
+        }
+    }
+}
